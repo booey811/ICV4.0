d480fc97b363568e9662aef044493a89e99de2a0
Gabriel Barr <gabrielbarr@Gabriels-iMac.local>
2020-08-20 18:06:14 +0100

    chaos

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+from flask import Flask
+
+
 from objects import Repair
 
 
+app = Flask(__name__)
 
-# How to add multiple pulses to one repair
+@app.route()
 
-test = Repair(vend="6ffc7cac-fb7b-9dfc-11ea-d59d7f1fc76d")
 
-print(test.name)
 
-test.debug_print()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    icv3.run(load_dotenv=True)
--- objects.py
+++ objects.py
@@ -1,27 +1,29 @@
 import json
-import requests
 import os
+import requests
 
 import settings
 import keys.vend
 import keys.monday
 
-from moncli import MondayClient
-
-
-# Differentiation between 'INITIALISATION' and 'INCLUSION'
-
+from moncli import MondayClient, create_column_value, ColumnType
 
 class Repair():
-    
+
     debug_string = []
     pulses = []
     vend = None
-    monday = None
-    zendesk = None    
-    
+    monday = []
+    zendesk = None
+
+    moncli = MondayClient(
+        user_name='systems@icorrect.co.uk',
+        api_key_v1=os.environ["MONV1SYS"],
+        api_key_v2=os.environ["MONV2SYS"]
+        )
+
     def __init__(self, vend=False, monday=False, zendesk=False, test=False):
-        
+
         if vend:
             self.include_vend(vend)
             self.source = "vend"
@@ -36,20 +38,25 @@ class Repair():
                     else:
                         self.number = number
 
-
         elif monday:
-            
+
             # Set self.monday, if another is addded, set to None or = self.pulses
-            
+
             self.include_monday(monday)
             self.source = "monday"
-        
+            self.name = self.monday[0].name
+            self.email = self.monday[0].email
+            self.number = self.monday[0].phone
+
         elif zendesk:
             self.source = "zendesk"
             self.include_zendesk(zendesk)
-            
+
         elif test:
             self.name = "Jeremiah Bullfrog"
+
+
+        self.query_applications()
 
     def include_vend(self, vend_sale_id):
 
@@ -59,65 +66,95 @@ class Repair():
     def include_monday(self, monday_id):
 
         self.debug("Adding[MONDAY] ID: {}".format(monday_id))
-        
-        if isinstance(self.monday, list):
-            self.monday.append(MondayRepair(monday_id))
-        elif self.monday:
-            self.monday = [self.monday, MondayRepair(monday_id)]
-        elif not self.monday:
-            self.monday = MondayRepair(monday_id)
-        
+
+        self.monday.append(MondayRepair(monday_id))
 
     def include_zendesk(self, zendesk_ticket_id):
-        
+
         self.debug("Adding[ZENDESK] ID: {}".format(zendesk_ticket_id))
         self.zendesk = ZendeskRepair(zendesk_ticket_id)
-        
+
+    def query_applications(self):
+
+        """Searches the specified applications for information related to the current repair"""
+
+        if self.source == 'monday':
+            if self.monday[0].v_id:
+                self.include_vend(self.monday[0].v_id)
+
+            for pulse in self.monday:
+                if pulse.z_ticket_id:
+                    self.include_zendesk(pulse.z_ticket_id)
+
+        elif self.source == 'vend':
+            col_val = create_column_value(id='text88', column_type=ColumnType.text, value=str(self.vend.id))
+            for item in self.moncli.get_board(id="349212843").get_items_by_column_values(col_val):
+                self.include_monday(item.id)
+
+            for pulse in self.monday:
+                if pulse.z_ticket_id:
+                    self.include_zendesk(pulse.z_ticket_id)
+                    break
+
+        elif self.source == 'zendesk':
+            col_val = create_column_value(id='text6', column_type=ColumnType.text, value=str(self.zendesk.ticket_id))
+            for item in self.moncli.get_board(id="349212843").get_items_by_column_values(col_val):
+                self.include_monday(item.id)
+
+            for pulse in self.monday:
+                if pulse.v_id:
+                    self.include_vend(pulse.v_id)
+                    break
+
+        else:
+            self.debug("Source of Repair not set")
+
+
     def debug(self, message, line=True, func_s=False, func_e=False):
-        
+
         if line:
             self.debug_string.append(message)
         elif func_s:
-            self.debug_string.append("$$$$  " + message + "  $$$$")            
+            self.debug_string.append("$$$$  " + message + "  $$$$")
         elif func_e:
             self.debug_string.append("//$$  " + message + "  $$\\\\")
-        
+
     def debug_print(self):
-        
+
         print("\n".join(self.debug_string))
-        
+
 class VendRepair(Repair):
-    
+
     def __init__(self, vend_sale_id):
-        
+
         self.id = vend_sale_id
         self.sale_info = self.query_for_sale()
         self.customer_id = str(self.sale_info["customer_id"])
         self.customer_info = self.query_for_customer()
-        
+
         self.passcode = None
         self.imeisn = None
-        
+
         self.pre_checks = []
         self.post_checks = []
         self.products = []
         self.notes = []
-        
+
         self.phone = self.customer_info["phone"]
         self.mobile = self.customer_info["mobile"]
         self.fax = self.customer_info["fax"]
         self.email = self.customer_info["email"]
-        
+
         self.client = "End User"
         self.service = "Walk-In"
         self.type = "Repair"
-        
+
         self.update_monday = False
-        
+
         self.get_and_organise_product_codes()
-    
+
     def query_for_customer(self):
-        
+
         url = "https://icorrect.vendhq.com/api/2.0/customers/{}".format(self.customer_id)
 
         headers = {'authorization': os.environ["VENDSYS"]}
@@ -125,11 +162,11 @@ class VendRepair(Repair):
         response = requests.request("GET", url, headers=headers)
 
         customer = json.loads(response.text)["data"]
-                
+
         return customer
-    
+
     def query_for_sale(self):
-        
+
         url = "https://icorrect.vendhq.com/api/2.0/sales/{}".format(self.id)
 
         headers = {'authorization': os.environ["VENDSYS"]}
@@ -137,37 +174,37 @@ class VendRepair(Repair):
         response = requests.request("GET", url, headers=headers)
 
         sale = json.loads(response.text)["data"]
-                
+
         return sale
-        
+
     def get_and_organise_product_codes(self):
-        
+
         """Go through products on sale organise into pre-checks, actual repairs, extract passcode/data/notification preferences"""
-        
+
         for product in self.sale_info["line_items"]:
-            
+
             # Check if 'Update Monday Product is present'
             if product["product_id"] == "549e099d-a641-7141-c907-cdd9d0266175":
                 self.update_monday = True
                 continue
-            
+
             # Check if Diagnostic Product is present, extract notes if so
             if product["product_id"] == "02d59481-b6ab-11e5-f667-e9f1a04c6e04":
                 self.type = "Diagnostic"
                 self.notes.append(product["note"])
                 continue
-            
+
             # Check if Warranty Product is present, extract notes if so
             if product["product_id"] == "02dcd191-aeab-11e6-f485-aea7f2c0a90a":
                 self.client = "Warranty"
                 self.notes.append(product["note"])
                 continue
-            
+
             # Check if product is the password product
             if product["product_id"] == "6ce9883a-dfd1-e137-1596-d7c3c97fb450":
                 self.passcode = product["note"]
                 continue
-            
+
             # Extract IMEI
             if product["note"]:
                 if any(option in product["note"] for option in ["IMEI", "SN", "S/N"]):
@@ -178,30 +215,29 @@ class VendRepair(Repair):
             if product["product_id"] in keys.vend.pre_checks:
                 self.pre_checks.append(keys.vend.pre_checks[product["product_id"]])
                 continue
-            
+
             # Add remaining codes to vend code attribute
             else:
                 self.products.append(product["product_id"])
-                      
+
 class MondayRepair(Repair):
-         
-    moncli = MondayClient(user_name='systems@icorrect.co.uk',
-                                   api_key_v1=os.environ["MONV1SYS"],
-                                   api_key_v2=os.environ["MONV2SYS"])
-    
+
+
+
+    v_id = None
+    z_ticket_id = None
+
     def __init__(self, monday_id):
-        
-        for item in self.moncli.get_items(limit=1, ids=[monday_id]):
+
+        for item in super().moncli.get_items(limit=1, ids=[int(monday_id)]):
             self.item = item
             break
-            
-        self.name = self.item.name.split()[0] + " " + self.item.name.split()[1]
-        
+
+        self.name = str(self.item.name.split()[0]) + " " + str(self.item.name.split()[1])
+
         self.retreive_column_data()
-        
+
         self.translate_column_data()
-        
-                
 
     def translate_column_data(self):
 
@@ -211,24 +247,24 @@ class MondayRepair(Repair):
                     ["Client", "m_client", "client"], ["Type", "m_type", "type"],
                     ["End Of Day", "m_eod", "eod"], ["ZenLink", "m_zenlink", "zenlink"],
                     ["Has Case", "m_has_case", "has_case"]]
-        
+
         for column, m_attribute, attribute in attributes:
 
             # Check in status column dictionary for the corresponding column
             for option in keys.monday.status_column_dictionary[column]["values"]:
-                
+
                 # Take title from column dictionary and add it to the Repair object
                 if option["index"] == getattr(self, m_attribute):
                     setattr(self, attribute, option["title"])
-                    
+
         self.debug("MONDAY translate_column_data", func_e=True)
 
-                       
+
     def retreive_column_data(self):
-        
-    
+
+
         column_values = self.item.get_column_values()
-        
+
         for item in keys.monday.col_ids_to_attributes:
             if keys.monday.col_ids_to_attributes[item]['attribute'] is not None:
                 col_id = item
@@ -246,27 +282,26 @@ class MondayRepair(Repair):
                             except TypeError:
                                 print(
                                     "*811*ERROR: TYPE: Cannot set {} Attribute in Class".format(keys.monday.col_ids_to_attributes[item]['attribute']))
-                                
+
 class ZendeskRepair(Repair):
-    
+
     def __init__(self, zendesk_ticket_number):
-        
+
         self.ticket_id = zendesk_ticket_number
-        
+
 class MonBoards():
-        
-    def __init__(self):        
-        
+
+    def __init__(self):
+
         self.boards = {}
-        
+
     # def add_board(self, username, board):
-        
-        
+
+
     #     self.boards[user_ids[usern]
 
-
 class PulseToAdd():
-    
+
     title_to_id = {
         'invoiced': 'check',
         'link_to_ticket': 'text410',
@@ -306,13 +341,13 @@ class PulseToAdd():
         'end_of_day': 'blocker',
         'deactivate': 'check71'
     }
-    
-    
-    
+
+
+
     def __init__(self):
-        
+
         self.name = None
-        
+
         self.invoiced = None
         self.link_to_ticket = None
         self.zenlink = None
@@ -351,14 +386,3 @@ class PulseToAdd():
         self.end_of_day = None
         self.deactivate = None
 
-# COMPARISONS & CORRECTIONS
-# comps = []
-# for item in [self.vend, self.monday, self.zendesk]:
-#   if item:
-#       comps.append(item)
-#
-# Compare this list
-
-
-# WALK IN CORPORATES
-# If user on zendesk and user has company - corporate
