b1b37f556f720620dc0f81dce6a1f4568ce56918
Gabriel Barr <gabrielbarr@Gabriels-iMac.local>
2020-08-21 20:11:53 +0100

    no idea

--- scratch_mod.py
+++ scratch_mod.py
@@ -2,19 +2,23 @@ from pprint import pprint as p
 import os
 
 from objects import Repair
+import settings
 
 from moncli import ColumnType, create_column_value, MondayClient
 
+item = Repair(monday=651224290)
+board = MondayClient(user_name="systems@icorrect.co.uk", api_key_v1=os.environ["MONV1SYS"], api_key_v2=os.environ["MONV2SYS"]).get_board(id=703218230)
 
-v1 = Repair(vend="6ffc7cac-fb7b-97b8-11ea-d17a810b420e")
+# for thing in board.get_items():
+#     for value in thing.get_column_values():
+#         print(value.__dict__)
 
-v2 = Repair(monday=667786416)
+print(ColumnType.dropdown.__dict__)
 
+for pulse in item.monday:
+    col_val = create_column_value(id="device0", column_type=ColumnType.dropdown, value=[2])
+    results = board.get_items_by_column_values(col_val)
+    print(len(results))
+    for item in results:
+        print(item.name)
 
-
-
-
-p(v1.__dict__)
-p(v2.__dict__)
-
-test = None
