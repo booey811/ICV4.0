0c0a8e7454c6551bbd296c0f8d5b1288fe8d10d5
Gabriel Barr <gabrielbarr811@gmail.com>
2020-12-23 11:03:17 +0000

    Stuart Data translated to rounded float

--- scratch.py
+++ scratch.py
@@ -1,4 +1,8 @@
-from objects import ScreenRefurb
+# from objects import ScreenRefurb
+#
+# screen = ScreenRefurb(user_id=user_id, item_id=int(data["event"]["pulseId"]))
+# screen.refurb_complete()
 
-screen = ScreenRefurb(user_id=user_id, item_id=int(data["event"]["pulseId"]))
-screen.refurb_complete()
+number = 31.45678
+
+print(round(float(number), 2))
